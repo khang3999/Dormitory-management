85afb23d6b79a006d957afab441502d9b6bb6bd9
VanDuPlus <22211TT1357@mail.tdc.edu.vn>
2024-07-06 20:32:08 +0700

    chucnangcuaVanDu

---
+++ app/Models/Student.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    public $timestamps = false;
+    use HasFactory;
+    protected $table = 'students';
+    protected $fillable = [
+        'name', 'MSSV', 'mail', 'gender', 'phone', 'cccd', 'birthday', 'address', 'nation', 'note', 'time', 'idphong'
+    ];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class, 'idphong');
+    }
+    
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($student) {
+            // Khi sinh viên được tạo, cập nhật lại số lượng sinh viên trong phòng
+            $student->updateRoomStatus();
+        });
+
+        static::deleted(function ($student) {
+            // Khi sinh viên bị xoá, cập nhật lại số lượng sinh viên trong phòng
+            $student->updateRoomStatus();
+        });
+    }
+
+    public function updateRoomStatus()
+    {
+        $room = $this->belongsTo(Room::class, 'idphong', 'id')->first();
+        if ($room) {
+            $room->status = $this->where('idphong', $room->id)->count();
+            $room->save();
+        }
+    }
+}
---
+++ database/migrations/2024_07_01_104243_create_students_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('avatar')->nullable();
+            $table->string('MSSV');
+            $table->string('password');
+            $table->string('name');
+            $table->string('cccd');
+            $table->string('birthday');
+            $table->string('phone');
+            $table->string('mail');
+            $table->string('nation');
+            $table->integer('gender');
+            $table->string('time');
+            $table->string('note')->nullable();
+            $table->string('address');
+            //type 0 là sinh viên out, 2 là sinh viên đăng kí vào, 1 là sinh viên hiện ở kí túc xá
+            $table->integer('type');
+            $table->string('idphong')->nullable();
+    
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+    
+};
